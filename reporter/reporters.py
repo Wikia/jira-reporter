6e0b0ddd769fbd3e177b987ab3520fec316d07a1
macbre <maciej.brencz@gmail.com>
2015-01-16 14:06:19 +0100

    Store report hash in the ticket description
    So we can then move the tickets between projects

--- reporter/reporters.py
+++ reporter/reporters.py
@@ -16,15 +16,20 @@ class Jira(object):
     @see http://jira-python.readthedocs.org/en/latest/
     """
 
-    JQL = "project = '{project}' AND {hash_field} ~ '{hash_value}'"
+    JQL = "{hash_field} ~ '{hash_value}' or summary ~ 'Hash: {hash_value}'"
 
     def __init__(self):
         self._logger = logging.getLogger('Jira')
         self._jira = JIRA(server=JIRA_CONFIG['url'], basic_auth=[JIRA_CONFIG['user'], JIRA_CONFIG['password']])
+
         self._fields = JIRA_CONFIG.get('fields')
         self._project = JIRA_CONFIG.get('project')
+        self._server = self._jira.client_info()
 
-        self._logger.info("Using {} project on <{}>".format(self._project, self._jira.client_info()))
+        self._logger.info("Using {} project on <{}>".format(self._project, self._server))
+
+    def _get_issue_url(self, issue_id):
+        return '{server}/browse/{issue_id}'.format(server=self._server, issue_id=issue_id)
 
     def ticket_exists(self, unique_id):
         """
@@ -36,9 +41,9 @@ class Jira(object):
         )
 
         if len(tickets) > 0:
-            self._logger.info('Found {count} tickets: {tickets}'.format(
+            self._logger.info('Found {count} tickets: <{tickets}>'.format(
                 count=len(tickets),
-                tickets=', '.join([ticket.key for ticket in tickets])
+                tickets='>, <'.join([self._get_issue_url(ticket.key) for ticket in tickets])
             ))
             return True
         else:
@@ -57,11 +62,18 @@ class Jira(object):
         if self.ticket_exists(report.get_unique_id()):
             return False
 
+        # add a hash and counter
+        description = report.get_description().strip()
+
+        description += '\n\n========================\nHash: {hash}\nOccurrences: {counter} in the last hour'.format(
+            hash=report.get_unique_id(), counter=report.get_counter()
+        )
+
         # it's not, create a ticket
         ticket_dict = {
             "project": {'key': self._project},
             "summary": report.get_summary()[:250],
-            "description": report.get_description(),
+            "description": description,
             "labels": [report.get_label()]
         }
 
@@ -71,8 +83,11 @@ class Jira(object):
         # set report_hash field
         ticket_dict[self._fields['custom']['unique_id']] = report.get_unique_id()
 
+        # report the ticket
         self._logger.info('Reporting {}'.format(json.dumps(ticket_dict)))
-        new_issue = self._jira.create_issue(fields=ticket_dict)
 
-        self._logger.info('Reported {}'.format(new_issue.key))
+        new_issue = self._jira.create_issue(fields=ticket_dict)
+        issue_id = new_issue.key
+
+        self._logger.info('Reported <{}>'.format(self._get_issue_url(issue_id)))
         return True
