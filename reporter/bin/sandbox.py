302f18cdcf3a8e058cfa316d42f9b45e28787416
macbre <maciej.brencz@gmail.com>
2015-02-18 20:21:42 -0800

    Set NotCachedWikiaApiResponsesSource threshold to 500 entries

--- reporter/bin/sandbox.py
+++ reporter/bin/sandbox.py
@@ -5,12 +5,12 @@ This script is a sandbox for testing new sources
 import logging
 from reporter.sources import NotCachedWikiaApiResponsesSource
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 reports = list()
 
 source = NotCachedWikiaApiResponsesSource()
-reports += source.query(threshold=1000)  # we serve 90k not cached responses
+reports += source.query(threshold=500)  # we serve 75k not cached responses an hour
 
 for report in reports:
     print report
