e371b5931e068df3f2ea4c5a55a4324e29d19032
macbre <maciej.brencz@gmail.com>
2015-07-31 16:28:02 +0200

    Import all PHP sources

--- reporter/sources/__init__.py
+++ reporter/sources/__init__.py
@@ -5,5 +5,5 @@ Export classes than will be used by "bin" scripts and tests
 from common import Source
 
 from caching import NotCachedWikiaApiResponsesSource
-from reporter.sources.php import *
+from php import *
 from pt_kill import KilledDatabaseQueriesSource
