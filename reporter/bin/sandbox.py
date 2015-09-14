cc703df88c65ef078201d271f35563e798f26071
macbre <maciej.brencz@gmail.com>
2015-09-14 12:06:42 +0200

    PHPExceptionsSource: add it to the list of sources

--- reporter/bin/sandbox.py
+++ reporter/bin/sandbox.py
@@ -4,7 +4,7 @@ This script is a sandbox for testing new sources
 """
 import logging
 from reporter.sources import KilledDatabaseQueriesSource, PHPErrorsSource, \
-    DBQueryNoLimitSource, DBQueryErrorsSource, PHPAssertionsSource, \
+    DBQueryNoLimitSource, DBQueryErrorsSource, PHPAssertionsSource, PHPExceptionsSource, \
     PandoraErrorsSource
 
 logging.basicConfig(level=logging.INFO)
@@ -26,9 +26,11 @@ reports += source.query(threshold=20)
 
 source = PHPAssertionsSource()
 reports += source.query(threshold=5)
-"""
 
 reports += PandoraErrorsSource().query(threshold=5)
+"""
+
+reports += PHPExceptionsSource().query(threshold=50)
 
 for report in reports:
     print report
