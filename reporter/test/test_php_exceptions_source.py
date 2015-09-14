ccce0a3a9c7ce0244c4c80a82886fa040605d85f
macbre <maciej.brencz@gmail.com>
2015-09-14 12:11:45 +0200

    PHPExceptionsSource: test _normalize function

---
+++ reporter/test/test_php_exceptions_source.py
@@ -0,0 +1,20 @@
+"""
+Set of unit tests for PHPExceptionsSource
+"""
+import unittest
+
+from ..sources import PHPExceptionsSource
+
+
+class PHPExceptionsSourceTestClass(unittest.TestCase):
+    """
+    Unit tests for PHPExceptionsSource class
+    """
+    def setUp(self):
+        self._source = PHPExceptionsSource()
+
+    def test_normalize(self):
+        assert self._source._normalize({'@message': 'Foo'}) == 'Production-None-Foo'
+        assert self._source._normalize({'@message': 'Foo', '@exception': {'class': 'Exception'}}) == 'Production-Exception-Foo'
+        assert self._source._normalize({'@message': 'Foo', '@exception': {'class': 'Exception', 'message': 'Bar'}}) == 'Production-Exception-Foo'
+        assert self._source._normalize({'@message': 'Foo', '@exception': {'class': 'WikiaException', 'message': 'Bar'}}) == 'Production-WikiaException-Bar'
