6aa1712f6f7a6d2172a2224ce9e63de90598de08
macbre <maciej.brencz@gmail.com>
2015-09-28 13:34:49 +0200

    Improve log format

--- reporter/bin/check.py
+++ reporter/bin/check.py
@@ -12,7 +12,7 @@ from reporter.sources import PHPErrorsSource, PHPExceptionsSource, DBQueryErrors
 
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s %(name)-25s %(levelname)-8s %(message)s',
+    format='%(asctime)s %(name)-35s %(levelname)-8s %(message)s',
     datefmt="%Y-%m-%d %H:%M:%S"
 )
 
