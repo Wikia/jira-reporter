4d7f2b1f3224c2c7666f0345e99e2ef38fb10352
macbre <maciej.brencz@gmail.com>
2016-09-05 17:03:43 +0200

    Move yaml to reporter/classifier/config/

--- reporter/bin/update_classifier_config.py
+++ reporter/bin/update_classifier_config.py
@@ -2,6 +2,8 @@
 This maintenace script is used to update Component name to Component ID mapping for tickets classifier
 
 It uses https://wikia-inc.atlassian.net/rest/api/2/project/MAIN/components API call
+
+Run it via "make update_classifier_config" from the base directory of this repository
 """
 import logging
 import yaml
@@ -13,6 +15,8 @@ logging.basicConfig(
     format='%(asctime)s %(name)-35s %(levelname)-8s %(message)s',
     datefmt="%Y-%m-%d %H:%M:%S"
 )
+
+CLASSIFIER_CONFIG_DIR = 'reporter/classifier/config/'
 
 
 def update_components_mapping():
@@ -28,7 +32,7 @@ def update_components_mapping():
     for component in jira_api.project_components('MAIN'):
         components[str(component.name.strip())] = int(component.id)
 
-    with open('reporter/classifier/components.yaml', mode='w') as fp:
+    with open(CLASSIFIER_CONFIG_DIR + 'components.yaml', mode='w') as fp:
         data = {
             'components': components
         }
@@ -37,13 +41,13 @@ def update_components_mapping():
         logger.info('{} generated'.format(fp.name))
 
 
-def update_extensions_mapping():
+def update_paths_mapping():
     """
-    Generate reporter/classifier/extensions.yaml with extensions / source code path to component name mapping
+    Generate reporter/classifier/paths.yaml with extensions / source code paths to component names mapping
     """
     pass
 
 
 if __name__ == '__main__':
     update_components_mapping()
-    update_extensions_mapping()
+    update_paths_mapping()
