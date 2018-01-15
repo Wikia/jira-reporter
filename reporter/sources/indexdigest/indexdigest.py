68918aab20c25a62fc146ccc016ea58fc6d51294
Maciej Brencz <maciej.brencz@gmail.com>
2018-01-15 09:32:11 +0100

    index_digest: do not report not_used_columns

--- reporter/sources/indexdigest/indexdigest.py
+++ reporter/sources/indexdigest/indexdigest.py
@@ -48,6 +48,7 @@ h6. Reported by {version} - https://github.com/macbre/index-digest#checks
         """
         return entry.get('report').get('type') not in [
             'non_utf_columns',
+            'not_used_columns',
         ]
 
     def _normalize(self, entry):
