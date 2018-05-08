733a4ea111d699f65296db2f69c662a52d000f92
Maciej Brencz <maciej.brencz@gmail.com>
2018-05-08 18:39:50 +0200

    Use wikia-common-kibana==2.2.4
    See https://github.com/macbre/wikia-common-kibana/pull/2

--- setup.py
+++ setup.py
@@ -15,9 +15,9 @@ setup(
     author_email='macbre@wikia-inc.com',
     install_requires=[
         'jira==0.32',
-        'pytest==2.6.4',
+        'pytest==3.4.0',
         'requests-oauthlib==0.4.2',
-        'wikia-common-kibana==2.2.3',
+        'wikia-common-kibana==2.2.4',
         'PyYAML==3.11',
     ],
     include_package_data=True,
