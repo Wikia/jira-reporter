170756a0a753fae85a8018bdaf5b9bc3e5007bb1
macbre <maciej.brencz@gmail.com>
2018-06-01 15:17:04 +0200

    Kibana: add columns list to the dashboard URL

--- reporter/test/test_kibana.py
+++ reporter/test/test_kibana.py
@@ -18,22 +18,22 @@ class KibanaSourceTestClass(unittest.TestCase):
         assert self._source.format_kibana_url('foo,bar') == self._source.format_kibana_url('foo bar')
 
         assert self._source.format_kibana_url('foo') == \
-               "https://kibana5.wikia-inc.com/app/kibana#/discover?_g=(time:(from:now-6h,mode:quick,to:now))&_a=(index:'logstash-other-*',query:(query_string:(analyze_wildcard:!t,query:'foo')),sort:!('@timestamp',desc))"
+               "https://kibana5.wikia-inc.com/app/kibana#/discover?_g=(time:(from:now-6h,mode:quick,to:now))&_a=(columns:!('@timestamp','@source_host','@message'),index:'logstash-other-*',query:(query_string:(analyze_wildcard:!t,query:'foo')),sort:!('@timestamp',desc))"
 
         assert self._source.format_kibana_url('foo bar') == \
-               "https://kibana5.wikia-inc.com/app/kibana#/discover?_g=(time:(from:now-6h,mode:quick,to:now))&_a=(index:'logstash-other-*',query:(query_string:(analyze_wildcard:!t,query:'foo%20bar')),sort:!('@timestamp',desc))"
+               "https://kibana5.wikia-inc.com/app/kibana#/discover?_g=(time:(from:now-6h,mode:quick,to:now))&_a=(columns:!('@timestamp','@source_host','@message'),index:'logstash-other-*',query:(query_string:(analyze_wildcard:!t,query:'foo%20bar')),sort:!('@timestamp',desc))"
 
         # TODO
         assert self._source.format_kibana_url('foo', ['@timestamp', 'name']) == \
-              "https://kibana5.wikia-inc.com/app/kibana#/discover?_g=(time:(from:now-6h,mode:quick,to:now))&_a=(index:'logstash-other-*',query:(query_string:(analyze_wildcard:!t,query:'foo')),sort:!('@timestamp',desc))"
+              "https://kibana5.wikia-inc.com/app/kibana#/discover?_g=(time:(from:now-6h,mode:quick,to:now))&_a=(columns:!('@timestamp','name'),index:'logstash-other-*',query:(query_string:(analyze_wildcard:!t,query:'foo')),sort:!('@timestamp',desc))"
 
         assert self._source.format_kibana_url(
             query='@exception.class: "Wikia\Security\Exception" AND @context.transaction: "foo/bar"'
-        ) == "https://kibana5.wikia-inc.com/app/kibana#/discover?_g=(time:(from:now-6h,mode:quick,to:now))&_a=(index:'logstash-other-*',query:(query_string:(analyze_wildcard:!t,query:'%40exception.class%3A%20%22Wikia%5C%5CSecurity%5C%5CException%22%20AND%20%40context.transaction%3A%20%22foo/bar%22')),sort:!('@timestamp',desc))"
+        ) == "https://kibana5.wikia-inc.com/app/kibana#/discover?_g=(time:(from:now-6h,mode:quick,to:now))&_a=(columns:!('@timestamp','@source_host','@message'),index:'logstash-other-*',query:(query_string:(analyze_wildcard:!t,query:'%40exception.class%3A%20%22Wikia%5C%5CSecurity%5C%5CException%22%20AND%20%40context.transaction%3A%20%22foo/bar%22')),sort:!('@timestamp',desc))"
 
         assert self._source.format_kibana_url(
             query='@exception.class: "Wikia\Util\AssertionException"'
-        ) == "https://kibana5.wikia-inc.com/app/kibana#/discover?_g=(time:(from:now-6h,mode:quick,to:now))&_a=(index:'logstash-other-*',query:(query_string:(analyze_wildcard:!t,query:'%40exception.class%3A%20%22Wikia%5C%5CUtil%5C%5CAssertionException%22')),sort:!('@timestamp',desc))"
+        ) == "https://kibana5.wikia-inc.com/app/kibana#/discover?_g=(time:(from:now-6h,mode:quick,to:now))&_a=(columns:!('@timestamp','@source_host','@message'),index:'logstash-other-*',query:(query_string:(analyze_wildcard:!t,query:'%40exception.class%3A%20%22Wikia%5C%5CUtil%5C%5CAssertionException%22')),sort:!('@timestamp',desc))"
 
     def test_get_env_from_entry(self):
         # main DC (SJC)
