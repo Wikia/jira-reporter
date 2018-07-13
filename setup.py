4ec9593d0df850cc3a827a854211ab321e458fcc
macbre <maciej.brencz@gmail.com>
2018-07-13 13:50:26 +0200

    Update dependencies

--- setup.py
+++ setup.py
@@ -15,10 +15,10 @@ setup(
     author_email='macbre@wikia-inc.com',
     install_requires=[
         'jira==0.32',
-        'pytest==3.4.0',
+        'pytest==3.6.3',
         'requests-oauthlib==0.4.2',
-        'wikia-common-kibana==2.2.4',
-        'PyYAML==3.11',
+        'wikia-common-kibana==2.2.5',
+        'PyYAML==3.13',
     ],
     include_package_data=True,
 )
