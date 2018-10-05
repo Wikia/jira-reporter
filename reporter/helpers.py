40c75774f39709295f2dfc52085dbfcc9ed569ba
macbre <maciej.brencz@gmail.com>
2018-10-05 09:36:53 +0200

    Linting helpers.py

--- reporter/helpers.py
+++ reporter/helpers.py
@@ -14,7 +14,7 @@ def is_from_production_host(entry):
     :rtype: bool
     """
     if isinstance(entry, str):
-        return re.search(r'^(ap|task|cron|job|liftium|staging|deploy|auth|staging-(ap|task))\-(s|r)', entry) is not None
+        return re.search(r'^(ap|task|cron|job|liftium|staging|deploy|auth|staging-(ap|task))-[sr]', entry) is not None
 
     # MediaWiki: @fields.environment   prod
     # Kubernetes: kubernetes.namespace_name	       	prod
@@ -61,7 +61,7 @@ def get_method_from_query(sql):
 
     Return: "Foo::bar"
     """
-    matches = re.search(r'/\*([^\*]+)\*/', sql)
+    matches = re.search(r'/\*([^*]+)\*/', sql)
 
     # return the first part of "Foo::Bar 157.55.39.174" (i.e. without IP)
     return matches.group(1).strip().split(' ')[0] if matches else None
